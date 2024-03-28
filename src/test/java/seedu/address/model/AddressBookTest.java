e2f8dc694e583fed36061d54dcd746e71b40820a
jocelyn-soh <s.sohjoce@gmail.com>
2024-03-29 01:27:54 +0800

    Add test cases

--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -10,6 +10,7 @@ import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -194,6 +195,96 @@ public class AddressBookTest {
 
         // Throws GroupNotFoundException when the non-existing group is removed
         assertThrows(GroupNotFoundException.class, () -> addressBook.removeGroup(nonExistingGroup));
+    }
+
+    @Test
+    public void setGroup_existingGroup_success() {
+        // Create an address book with an existing group
+        AddressBook addressBook = new AddressBook();
+        Group existingGroup = new Group("TUT04");
+        addressBook.addGroup(existingGroup);
+
+        // Create a new group
+        Group newGroup = new Group("TUT01");
+
+        // Replace the existing group with the new group
+        addressBook.setGroup(existingGroup, newGroup);
+
+        // Check that the new group is in the address book
+        assertFalse(addressBook.getGroupList().contains(existingGroup));
+        assertTrue(addressBook.getGroupList().contains(newGroup));
+    }
+
+    @Test
+    public void setGroup_nonExistingGroup_noChange() {
+        // Create an address book with an existing group
+        AddressBook addressBook = new AddressBook();
+        Group existingGroup = new Group("TUT04");
+        addressBook.addGroup(existingGroup);
+
+        // Create a new group
+        Group nonExistingGroup = new Group("TUT01");
+
+        // Throws GroupNotFoundException when the non-existing group is replaced
+        assertThrows(GroupNotFoundException.class, () -> addressBook.setGroup(nonExistingGroup, existingGroup));
+    }
+
+    @Test
+    public void equals_sameInstance_returnsTrue() {
+        AddressBook addressBook = new AddressBook();
+        assertTrue(addressBook.equals(addressBook));
+    }
+
+    @Test
+    public void equals_null_returnsFalse() {
+        AddressBook addressBook = new AddressBook();
+        assertFalse(addressBook.equals(null));
+    }
+
+    @Test
+    public void equals_differentClass_returnsFalse() {
+        AddressBook addressBook = new AddressBook();
+        assertFalse(addressBook.equals("Not an AddressBook instance"));
+    }
+
+    @Test
+    public void equals_differentPersons_returnsFalse() {
+        AddressBook addressBook1 = new AddressBook();
+        AddressBook addressBook2 = new AddressBook();
+
+        // Add a person to the first address book
+        Person person1 = new PersonBuilder().withName("Alice").withGroups("TUT05").build();
+        addressBook1.addPerson(person1);
+
+        // Add a person to the second address book
+        Person person2 = new PersonBuilder().withName("Bob").withGroups("LAB08").build();
+        addressBook2.addPerson(person2);
+        assertFalse(addressBook1.equals(addressBook2));
+    }
+
+    @Test
+    public void equals_differentGroups_returnsFalse() {
+        AddressBook addressBook1 = new AddressBook();
+        AddressBook addressBook2 = new AddressBook();
+        addressBook1.addGroup(new Group("TUT01"));
+        addressBook2.addGroup(new Group("LAB01"));
+        assertFalse(addressBook1.equals(addressBook2));
+    }
+
+    @Test
+    public void equals_sameContent_returnsTrue() {
+        AddressBook addressBook1 = new AddressBook();
+        AddressBook addressBook2 = new AddressBook();
+        List<Person> persons = new ArrayList<>();
+        List<Group> groups = new ArrayList<>();
+        Person person1 = new PersonBuilder().withName("Alice").withGroups("TUT05").build();
+        addressBook1.addPerson(person1);
+        groups.add(new Group("TUT05"));
+        addressBook1.setPersons(persons);
+        addressBook2.setPersons(persons);
+        addressBook1.setGroups(groups);
+        addressBook2.setGroups(groups);
+        assertTrue(addressBook1.equals(addressBook2));
     }
 
     /**
